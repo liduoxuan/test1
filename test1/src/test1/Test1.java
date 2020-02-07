8e251deac210f4df04647008bf81500562dd69e6
liduoxuan <761075623@qq.com>
2020-02-07 14:25:58 +0800

    2

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -1,10 +1,9 @@
 package test1;
 
 public class Test1 {
-
+     static int b;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("1");
+		System.out.println(b);
 	}
 
 }
